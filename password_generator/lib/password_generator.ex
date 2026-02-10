c138a0d6221b2f86af545e6135720f91a897de24
ha165 <lumumbaharmony@gmail.com>
2026-02-10 14:29:55 +0300

    Add documentation

--- password_generator/lib/password_generator.ex
+++ password_generator/lib/password_generator.ex
@@ -1,6 +1,15 @@
 defmodule PasswordGenerator do
   @moduledoc """
-  Documentation for `PasswordGenerator`.
+  Generates random password depending on parameteres,modulle main functionis `generate(options)`  
+  That functions takes the options map.
+  Options Example:
+  options =%{
+  "length" => "5",
+  numbers:"false",
+  symbols:"false",
+  uppercase:"false",
+  }
+  The options are only 4.`length` is the length of the password, and the other 3 options are for including numbers, symbols and uppercase letters in the password.
   """
 
   @doc """
@@ -12,7 +21,4 @@ defmodule PasswordGenerator do
       :world
 
   """
-  def hello do
-    :world
-  end
 end
